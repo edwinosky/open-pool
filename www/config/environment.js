7e85dd95c882869be962c93c5e0e2bcc7eeb692c
Edwin Corleone <edwinpitters@yahoo.com>
2019-06-17 17:06:40 -0400

    Update environment.js

--- www/config/environment.js
+++ www/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'open-social-pool',
+    modulePrefix: 'open-pool',
     environment: environment,
     rootURL: '/',
     locationType: 'hash',
